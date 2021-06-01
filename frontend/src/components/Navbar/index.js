a33c944ed35af238749f2461e9c163a49b6d878c
THEMUD <themud2001@hotmail.com>
2021-06-01 23:43:51 +0300

    Removed the additional Toolbar component :

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -16,56 +16,52 @@ const Navbar = () => {
     const classes = useStyles();
 
     return (
-        <React.Fragment>
-            <AppBar className={classes.root}>
-                <Toolbar>
+        <AppBar className={classes.root}>
+            <Toolbar>
+                <Grid
+                    container
+                    direction="row"
+                    alignItems="center"
+                >
                     <Grid
+                        item
                         container
-                        direction="row"
+                        xs={6}
                         alignItems="center"
                     >
-                        <Grid
-                            item
-                            container
-                            xs={6}
-                            alignItems="center"
-                        >
-                            <Grid item>
-                                <IconButton className={classes.menuIcon}>
-                                    <Menu />
-                                </IconButton>
-                            </Grid>
-
-                            <Grid item>
-                                <Typography
-                                    component={Link}
-                                    to="/"
-                                    variant="h5"
-                                    className={classes.title}
-                                >Talky</Typography>
-                            </Grid>
+                        <Grid item>
+                            <IconButton className={classes.menuIcon}>
+                                <Menu />
+                            </IconButton>
                         </Grid>
 
-                        <Grid
-                            item
-                            container
-                            xs={6}
-                            justify="flex-end"
-                        >
-                            <Grid item>
-                                <Button
-                                    component={Link}
-                                    to="/login"
-                                    className={classes.loginButton}
-                                >LOGIN</Button>
-                            </Grid>
+                        <Grid item>
+                            <Typography
+                                component={Link}
+                                to="/"
+                                variant="h5"
+                                className={classes.title}
+                            >Talky</Typography>
                         </Grid>
                     </Grid>
-                </Toolbar>
-            </AppBar>
 
-            <Toolbar />
-        </React.Fragment>
+                    <Grid
+                        item
+                        container
+                        xs={6}
+                        justify="flex-end"
+                    >
+                        <Grid item>
+                            <Button
+                                component={Link}
+                                to="/login"
+                                className={classes.loginButton}
+                            >LOGIN</Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Toolbar>
+        </AppBar>
     );
 };
 
