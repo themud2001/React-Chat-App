68a29dc9f4fd2c89ace0619ce1e3acd7d2897a0f
THEMUD <themud2001@hotmail.com>
2021-06-02 12:48:25 +0300

    Added an additional Toolbar component to margin the content of the page down a little bit

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -16,52 +16,56 @@ const Navbar = () => {
     const classes = useStyles();
 
     return (
-        <AppBar className={classes.root}>
-            <Toolbar>
-                <Grid
-                    container
-                    direction="row"
-                    alignItems="center"
-                >
+        <React.Fragment>
+            <AppBar className={classes.root}>
+                <Toolbar>
                     <Grid
-                        item
                         container
-                        xs={6}
+                        direction="row"
                         alignItems="center"
                     >
-                        <Grid item>
-                            <IconButton className={classes.menuIcon}>
-                                <Menu />
-                            </IconButton>
+                        <Grid
+                            item
+                            container
+                            xs={6}
+                            alignItems="center"
+                        >
+                            <Grid item>
+                                <IconButton className={classes.menuIcon}>
+                                    <Menu />
+                                </IconButton>
+                            </Grid>
+
+                            <Grid item>
+                                <Typography
+                                    component={Link}
+                                    to="/"
+                                    variant="h5"
+                                    className={classes.title}
+                                >Talky</Typography>
+                            </Grid>
                         </Grid>
 
-                        <Grid item>
-                            <Typography
-                                component={Link}
-                                to="/"
-                                variant="h5"
-                                className={classes.title}
-                            >Talky</Typography>
+                        <Grid
+                            item
+                            container
+                            xs={6}
+                            justify="flex-end"
+                        >
+                            <Grid item>
+                                <Button
+                                    component={Link}
+                                    to="/login"
+                                    className={classes.loginButton}
+                                >LOGIN</Button>
+                            </Grid>
                         </Grid>
                     </Grid>
+                </Toolbar>
+            </AppBar>
 
-                    <Grid
-                        item
-                        container
-                        xs={6}
-                        justify="flex-end"
-                    >
-                        <Grid item>
-                            <Button
-                                component={Link}
-                                to="/login"
-                                className={classes.loginButton}
-                            >LOGIN</Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Toolbar>
-        </AppBar>
+            <Toolbar />
+        </React.Fragment>
     );
 };
 
