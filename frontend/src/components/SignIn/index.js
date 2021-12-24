13ffeb739d03b6140c431fd5ada8c78239f76547
THEMUD <themud2001@hotmail.com>
2021-12-25 00:06:55 +0200

    Almost finisehd the SignIn component and edited the SignUp component

--- frontend/src/components/SignIn/index.js
+++ frontend/src/components/SignIn/index.js
@@ -1,7 +1,30 @@
 import React from "react";
+import { useForm } from "react-hook-form";
+
+import "./styles.scss";
 
 const SignIn = () => {
-    return <div>SignIn Component</div>;
+    const { register, handleSubmit } = useForm();
+    const onFormSubmit = data => {
+        console.log(data);
+    };
+
+    return (
+        <div className="signin-wrapper">
+            <div className="container">
+                <h1>Sign In</h1>
+                <form onSubmit={handleSubmit(onFormSubmit)}>
+                    <label htmlFor="email">E-mail:</label>
+                    <input id="email" type="text" {...register("email")} />
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" {...register("password")} />
+
+                    <button type="submit">Sign In</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default SignIn;
--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -11,37 +11,45 @@ const SignUp = () => {
     };
 
     const emailValidation = {
-        required: true,
-        pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+        required: "E-mail is required",
+        pattern: {
+            value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            message: "Invalid E-mail"
+        }
     };
 
     const passwordValidation = {
-        required: true,
-        minLength: 8,
-        maxLength: 50
+        required: "Password is required",
+        minLength: {
+            value: 8,
+            message: "Must be more than 8 characters"
+        },
+        maxLength: {
+            value: 50,
+            message: "Must be less than 50 characters"
+        }
     };
 
     const confirmValidation = {
-        required: true,
-        validate: value => value === watchPassword
+        validate: value => value === watchPassword || "Passwords do not match"
     };
 
     return (
         <div className="signup-wrapper">
             <div className="container">
-                <h1>Sign Up</h1>
+                <h1>Create an Account</h1>
                 <form onSubmit={handleSubmit(onFormSubmit)}>
                     <label htmlFor="email">E-mail:</label>
                     <input id="email" type="text" {...register("email", emailValidation)} />
-                    {errors.email && <span className="error">Invalid E-mail</span>}
+                    {errors.email && <span className="error">{errors.email.message}</span>}
 
                     <label htmlFor="password">Password:</label>
                     <input id="password" type="password" {...register("password", passwordValidation)} />
-                    {errors.password && <span className="error">Invalid Password</span>}
+                    {errors.password && <span className="error">{errors.password.message}</span>}
 
                     <label htmlFor="confirm-password">Confirm Password:</label>
                     <input id="confirm-password" type="password" {...register("confirm-password", confirmValidation)} />
-                    {errors["confirm-password"] && <span className="error">Passwords do not match</span>}
+                    {errors["confirm-password"] && <span className="error">{errors["confirm-password"].message}</span>}
 
                     <button type="submit">Sign Up</button>
                 </form>
