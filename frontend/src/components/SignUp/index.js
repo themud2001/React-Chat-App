1dd94bf0782755cbd19bc00ce1363c0be4039ebc
THEMUD <themud2001@hotmail.com>
2021-12-24 14:18:00 +0200

    Designing the form of the SignUp component

--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+import "./styles.scss";
+
 const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const onFormSubmit = data => console.log(data);
 
     return (
-        <div>
-            
+        <div className="signup-wrapper">
+            <div className="container">
+                <h1>Sign Up Form</h1>
+                <form onSubmit={handleSubmit(onFormSubmit)}>
+                    <label htmlFor="email">E-mail:</label>
+                    <input id="email" />
+
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" />
+
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
         </div>
     );
 };
