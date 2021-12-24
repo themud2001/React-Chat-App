729954306d1e858aedb1fa2a40db73d21692b340
THEMUD <themud2001@hotmail.com>
2021-12-24 23:13:44 +0200

    Almost finisehd the SignUp component

--- frontend/src/components/SignUp/index.js
+++ frontend/src/components/SignUp/index.js
@@ -4,19 +4,44 @@ import { useForm } from "react-hook-form";
 import "./styles.scss";
 
 const SignUp = () => {
-    const { register, formState: { errors }, handleSubmit } = useForm();
-    const onFormSubmit = data => console.log(data);
+    const { register, formState: { errors }, handleSubmit, watch } = useForm({ mode: "onBlur" });
+    const watchPassword = watch("password");
+    const onFormSubmit = data => {
+        console.log(data);
+    };
+
+    const emailValidation = {
+        required: true,
+        pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    };
+
+    const passwordValidation = {
+        required: true,
+        minLength: 8,
+        maxLength: 50
+    };
+
+    const confirmValidation = {
+        required: true,
+        validate: value => value === watchPassword
+    };
 
     return (
         <div className="signup-wrapper">
             <div className="container">
-                <h1>Sign Up Form</h1>
+                <h1>Sign Up</h1>
                 <form onSubmit={handleSubmit(onFormSubmit)}>
                     <label htmlFor="email">E-mail:</label>
-                    <input id="email" />
+                    <input id="email" type="text" {...register("email", emailValidation)} />
+                    {errors.email && <span className="error">Invalid E-mail</span>}
 
                     <label htmlFor="password">Password:</label>
-                    <input id="password" />
+                    <input id="password" type="password" {...register("password", passwordValidation)} />
+                    {errors.password && <span className="error">Invalid Password</span>}
+
+                    <label htmlFor="confirm-password">Confirm Password:</label>
+                    <input id="confirm-password" type="password" {...register("confirm-password", confirmValidation)} />
+                    {errors["confirm-password"] && <span className="error">Passwords do not match</span>}
 
                     <button type="submit">Sign Up</button>
                 </form>
