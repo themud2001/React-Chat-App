5cf9472ff7196accfe4aa44f12517071bd0ed657
THEMUD <themud2001@hotmail.com>
2021-12-24 03:17:52 +0200

    Changed the routing structure for the app

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,16 +1,9 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./styles.scss";
-import Navbar from "../Navbar";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Navbar />
-            <div>App component</div>
-        </BrowserRouter>
-    );
+    return <div>App component</div>;
 };
 
 export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
 import App from "./components/App";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
 ReactDOM.render(
-    <App />,
+    <BrowserRouter>
+        <Navbar />
+        
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+        </Routes>
+    </BrowserRouter>,
     document.getElementById("root")
 );
