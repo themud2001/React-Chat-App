70fda0ea59b605241ab2a13b84ec6e6293db8132
THEMUD <themud2001@hotmail.com>
2021-12-27 23:11:03 +0200

    Modified axios to include the credentials in requests, and allowed them on the API server

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,10 @@ const errorHandler = require("./middleware/errorHandler");
 
 app.use(express.json());
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
 app.use(xss());
 app.use(cookieParser());
 app.use(compression());
--- frontend/src/apis/api.js
+++ frontend/src/apis/api.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3090"
+    baseURL: "http://localhost:3090",
+    withCredentials: true
 });
