ae716ea392fea82a5d5310f126eb2df710e99b82
THEMUD <themud2001@hotmail.com>
2021-12-28 00:47:39 +0200

    Added conditional rendering

--- frontend/src/components/Navbar/index.js
+++ frontend/src/components/Navbar/index.js
@@ -1,21 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
 import "./styles.scss";
 
-const Navbar = () => {
+const Navbar = ({ token }) => {
     return ReactDOM.createPortal(
         <>
             <Link className="logo" to="/">Talky</Link>
 
-            <ul className="nav-links">
-                <li><Link to="/signin">Sign In</Link></li>
-                <li><Link to="/signup">Sign Up</Link></li>
-            </ul>
+            {!token &&
+                <ul className="nav-links">
+                    <li><Link to="/signin">Sign In</Link></li>
+                    <li><Link to="/signup">Sign Up</Link></li>
+                </ul>
+            }
         </>,
         document.querySelector(".navbar")
     );
 };
 
-export default Navbar;
+const mapStateToProps = state => {
+    return {
+        token: state.auth.token,
+        user: state.auth.user
+    };
+};
+
+export default connect(mapStateToProps)(Navbar);
