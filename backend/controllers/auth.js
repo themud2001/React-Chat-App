52856c661cbd493dbbd67983713ddffe2ee39f5f
THEMUD <themud2001@hotmail.com>
2021-12-27 02:18:03 +0200

    Finished setting up the authentication routes/controllers

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -2,8 +2,31 @@ const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
-exports.signIn = (req, res, next) => {
-    res.send("Sign in");
+exports.signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    if (
+        !email ||
+        !password ||
+        email.trim() === "" ||
+        password.trim() === ""
+    ) {
+        return res.status(400).json({ errorMessage: "Invalid credentials" });
+    }
+
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(400).json({ errorMessage: "User does not exist" });
+
+        const isMatch = await user.comparePassword(password);
+
+        if (!isMatch) return res.status(400).json({ errorMessage: "Invalid credentials" });
+
+        return res.status(200).json({ token: user.getToken() });
+    } catch (err) {
+        return next(err);
+    }
 };
 
 exports.signUp = async (req, res, next) => {
