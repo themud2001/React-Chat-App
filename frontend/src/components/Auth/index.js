5c44cf892e534283876d4ddcd0f5ed903d1a48e1
THEMUD <themud2001@hotmail.com>
2021-12-29 00:44:52 +0200

    Implemented silent refreshing to renew the access token every 14 minutes, 1 minute less than the actual expiry

--- frontend/src/components/Auth/index.js
+++ frontend/src/components/Auth/index.js
@@ -6,6 +6,16 @@ import { refreshToken } from "../../redux/actions";
 const Auth = ({ refreshToken }) => {
     useEffect(() => {
         refreshToken();
+
+        // A timer that refreshes the token automatically
+        // every 14 minutes (1 minute less than the expiry of the access token)
+        const timer = setInterval(() => {
+            refreshToken();
+        }, 14 * 60 * 1000);
+
+        return () => {
+            clearInterval(timer);
+        }
     }, []);
 
     return null;
